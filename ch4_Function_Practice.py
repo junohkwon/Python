8d70fa9e69b7b9d5e6ce695f980d9b88d1f489b1
DS2 <jokwon79@gmail.com>
2018-07-18 09:31:30 +0900

    Merge branch 'master' of https://github.com/junohkwon/Python
    # Conflicts:
    #	.idea/misc.xml

--- ch4_Function_Practice.py
+++ ch4_Function_Practice.py
@@ -1,6 +1,5 @@
 '''
-ch0. Homework
-Function Practice
+4-A.Function Practice (10조)
 '''
 
 def f2(lst):
@@ -8,9 +7,6 @@ def f2(lst):
         if lst[i] % 2 ==1:
             print(lst[i])
 
-def f2_new(lst):
-    for x in filter(lambda x : x%2, lst):
-        print(x)
 
 
 def f4(lst):
@@ -20,10 +16,6 @@ def f4(lst):
             sumval += i
     return sumval
 
-from functools import reduce
-def f4_new(lst):
-    newlst = list(filter(lambda x : x%2, lst))
-    return reduce(lambda x,y:x+y, newlst)
 
 
 def f6(lst):
@@ -33,8 +25,6 @@ def f6(lst):
             maxval = lst[i]
     return maxval
 
-def f6_new(lst):
-    return max(lst)
 
 
 def f8(a,b,n):
@@ -42,9 +32,6 @@ def f8(a,b,n):
         if i % n == 0:
             print(i)
 
-def f8_new(a,b,n):
-    for i in filter(lambda x:x%n == 0, range(a,b+1)):
-        print(i)
 
 
 def f10(n):
@@ -52,6 +39,8 @@ def f10(n):
         for j in range(0,i):
             print('*',end='')
         print()
+
+
 
 def f12(lst):
     negative_cnt = 0
@@ -64,6 +53,8 @@ def f12(lst):
     else:
         return False
 
+
+
 def f14(lst):
     max_idx=-1
     for i in range(0,len(lst)):
@@ -72,11 +63,15 @@ def f14(lst):
 
     return max_idx
 
+
+
 def f16(n):
     for i in range(n,0,-1):
         for j in range(i,0,-1):
             print('*',end='')
         print()
+
+
 
 def f18(n):
     fac = 1
@@ -85,16 +80,22 @@ def f18(n):
 
     return fac
 
+
+
 def f20(lst):
     for row in range(0,len(lst)):
         for i in range(lst[row],-1,-1):
             print(i,end=' ')
         print()
 
+
+
 def f22(n):
     for i in range(1,n+1):
         if i%2 == 0 or i%3 == 0:
             print(i)
+
+
 
 def f24(lst):
     fir_max= -99999
@@ -109,6 +110,8 @@ def f24(lst):
             sec_max = val
     return sec_max
 
+
+
 def f26(lst):
     for row in range(0,len(lst)):
         maxval = lst[row][0]
@@ -117,67 +120,3 @@ def f26(lst):
                 maxval = lst[row][col]
 
         print(maxval)
-
-print('====== f2 ======')
-f2([1,2,3,4])
-f2([1,2,3,4,5])
-
-print('====== f4 ======')
-print(f4([1,2,3,4]))
-print(f4([1,2,3,4,5]))
-
-print('====== f6 ======')
-print(f6([1,2,3,4]))
-print(f6([1,2,3,4,5]))
-
-print('====== f8 ======')
-f8(1,10,2)
-f8(1,10,11)
-f8(1,10,7)
-
-print('====== f10 ======')
-f10(1)
-f10(2)
-f10(3)
-
-print('====== f12 ======')
-print(f12([]))
-print(f12([-1,-2,-3,-4,5]))
-print(f12([1,2,3,4,5]))
-print(f12([-1,-2,-3]))
-
-print('====== f14 ======')
-print(f14([1,2,-3]))
-print(f14([1,-2,-3,1,-2,-3]))
-print(f14([-1,1,1,1]))
-
-print('====== f16 ======')
-f16(3)
-f16(2)
-f16(1)
-
-print('====== f18 ======')
-print(f18(0))
-print(f18(2))
-print(f18(3))
-
-print('====== f20 ======')
-f20([])
-f20([1,3,5])
-f20([5,3,6,2])
-
-print('====== f22 ======')
-f22(10)
-f22(1)
-f22(3)
-
-print('====== f24 ======')
-print(f24([1,4,3,2,5]))
-print(f24([3,2]))
-print(f24([3,4]))
-
-print('====== f26 ======')
-f26([[1,2,3],[4,5,6],[7,8,9]])
-f26([[3,2,1],[0,-1,-2]])
-f26([[1,2,3,4],[1],[34],[2],[3],[56],[67]])
-
