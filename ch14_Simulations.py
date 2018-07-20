bbd01ba737ab7ec03b0ba4610992af3108c6856b
DS2 <jokwon79@gmail.com>
2018-07-20 12:00:00 +0900

    Merge branch 'master' of https://github.com/junohkwon/Python
    # Conflicts:
    #	.idea/misc.xml

---
+++ ch14_Simulations.py
@@ -0,0 +1,69 @@
+'''
+hungry dice game
+'''
+
+def roll():
+    from random import randint
+    return randint(1,6)
+
+def dice_game():
+	strikes=0
+	winnings=0
+	while strikes < 3:
+		die1 = roll()
+		die2 = roll()
+		if die1 == die2:
+			strikes = strikes + 1
+		else:
+			winnings = winnings + die1 + die2
+	return winnings
+
+'''
+clueless student problem
+'''
+
+def average_winnings(runs):
+    total=0
+    for n in range(runs):
+        total = total + dice_game()
+
+    return total/runs
+
+[round(average_winnings(10000),2) for i in range(5)]
+
+
+import random as r
+def student(pairs, samples):
+    num_correct = 0
+    matching = list(range(pairs))
+    for i in range(samples):
+        r.shuffle(matching)
+        for j in range(pairs):
+            if matching[j] == j:
+                num_correct = num_correct + 1
+    return num_correct / samples
+
+'''
+Umbrella Quandary problem
+'''
+
+from random import random
+
+def umbrella(p):
+    wet = False
+    trips = 0
+    location = 0
+    umbrellas = [1,1]
+    while (not wet):
+        if random() < p :
+            if umbrellas[location] == 0:
+                wet = True
+            else:
+                trips = trips + 1
+                umbrellas[location] -= 1
+                location = 1 - location
+                umbrellas[location] += 1
+        else:
+            trips += 1
+            location = 1-location
+    return trips
