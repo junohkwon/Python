9928436cb0d009f34ea76be2c790d9453844457e
DS2 <jokwon79@gmail.com>
2018-07-24 12:08:52 +0900

    Merge branch 'master' of https://github.com/junohkwon/Python
    # Conflicts:
    #	.idea/misc.xml

--- ch10_Function_Practice_Sophistication.py
+++ ch10_Function_Practice_Sophistication.py
@@ -1,103 +1,42 @@
-# def f2(lst):
-#     print(*list(filter(lambda x : x%2 == 1, lst)),sep='\n')
-#
-# a = [1,2,3,4]
-# b=[1,2,3,4,5]
-# f2(a)
-# f2(b)
+def f2(lst):
+    print(*list(filter(lambda x : x%2 == 1, lst)),sep='\n')
 
-# def f4(lst):
-#     print(sum(list(map(lambda x : x[0] if x[1]%2==1 else 0,enumerate(lst)))))
-#
-# f4([1,2,3,4])
-# f4([1,2,3,4,5])
+def f4(lst):
+    return sum(list(map(lambda x : x[0] if x[1]%2==1 else 0,enumerate(lst))))
 
-# def f6(lst):
-#     print(max(lst))
-#
-# f6(a)
-# f6(b)
+def f6(lst):
+    return max(lst)
 
-# def f8(a,b,n):
-#     print(*(filter(lambda x : x%n==0, range(a,b+1))),sep='\n')
-#
-# f8(1,10,2)
-# f8(1,10,11)
-# f8(1,10,7)
+def f8(a,b,n):
+    print(*(filter(lambda x : x%n==0, range(a,b+1))),sep='\n')
 
-# def f10(n):
-#     print(*(map(lambda x : '*'*x, range(1,n+1))),sep='\n')
-#
-# f10(1)
-# f10(2)
-# f10(3)
+def f10(n):
+    print(*(map(lambda x : '*'*x, range(1,n+1))),sep='\n')
 
-# def f12(lst):
-#     print(all(map(lambda x : x < 0, lst)))
-#
-# f12([])
-# f12([-1,-2,-3,-4,5])
-# f12([1,2,3,4,5])
-# f12([-1,-2,-3])
+def f12(lst):
+    return all(map(lambda x : x < 0, lst))
 
-# def f14(lst):
-#     return list(filter(lambda x:x[1]<0, enumerate(lst)))[-1][0]
-#
-# a=[1,2,-3]
-# b=[1,-2,-3,1,-2,-3]
-# c=[-1,1,1,1]
-# f14(a)
-# f14(b)
-# f14(c)
+def f14(lst):
+    return list(filter(lambda x:x[1]<0, enumerate(lst)))[-1][0]
 
-# def f16(n):
-#     print(*(map(lambda x : '*'*x, range(n,0,-1))),sep='\n')
-#
-# f16(3)
-# f16(2)
-# f16(1)
+def f16(n):
+    print(*(map(lambda x : '*'*x, range(n,0,-1))),sep='\n')
 
-# import math
-# def f18(n):
-#     return math.factorial(n)
-#
-# f18(0)
-# f18(2)
-# f18(3)
+def f18(n):
+    import math
+    return math.factorial(n)
 
-# def f20(matrix):
-#     #print(*list(map(lambda x:x[1][x[0]], enumerate(matrix))),sep='\n')
-#     pass
-#
-# #enumerate(matrix) ==> [(0,[1,2,3]),(1,[4,5,6]),(2,[7,8,9])]
-# #x[1] = x[x[0]]
-#
-# # lambda x:matrix[x][x], range(matrix)
-#
-# f20([[1,2,3],[4,5,6],[7,8,9]])
+def f20(matrix):
+    print(*list(map(lambda x:x[1][x[0]], enumerate(matrix))),sep='\n')
 
-# def f22(lst):
-#     print(*list(map(lambda x:list(range(x,-1,-1)),lst)),sep='\n')
-#
-# f22([1,3,5])
+def f22(lst):
+    print(*list(map(lambda x:list(range(x,-1,-1)),lst)),sep='\n')
 
-# def f24(n):
-#     print(*filter(lambda x : x%2==0 or x%3==0, range(1,n+1)),sep='\n')
-#
-# f24(10)
-# f24(1)
-# f24(3)
+def f24(n):
+    print(*filter(lambda x : x%2==0 or x%3==0, range(1,n+1)),sep='\n')
 
-# def f26(lst):
-#     print(sorted(lst)[-2])
-#
-# f26([1,4,3,2,5])
-# f26([3,2])
-# f26([3,4])
+def f26(lst):
+    return sorted(lst)[-2]
 
-# def f28(lst):
-#     print(*map(lambda x : sorted(x[1])[-1], enumerate(lst)),sep='\n')
-#
-# f28([[1,2,3],[4,5,6],[7,8,9]])
-# f28([[3,2,1],[0,-1,-2]])
-# f28([[1,2,3,4],[1],[34],[2],[3],[56],[67]])
+def f28(lst):
+    print(*map(lambda x : sorted(x[1])[-1], enumerate(lst)),sep='\n')
--- ch10_LoopingMatrix_Practice_Sophistication.py
+++ ch10_LoopingMatrix_Practice_Sophistication.py
@@ -1,68 +1,60 @@
-# def f2(n):
-#     sum=1;
-#     for i in range(1,n+1):
-#         print(*list(map(lambda x:x, range(sum,sum+i))))
-#         sum += i
-#
-# f2(3)
-# f2(0)
-# f2(5)
+def f2(n):
+    sum=1;
+    for i in range(1,n+1):
+        print(*list(map(lambda x:x, range(sum,sum+i))))
+        sum += i
 
-# def f4(n):
-#     sum=1;
-#     for loop in range(0,2):
-#         for i in range(1,n+1):
-#             if(loop == 0):
-#                 print(*list(map(lambda x: x, range(sum, sum + i))))
-#                 sum += i
-#             else:
-#                 print(*list(map(lambda x: x, range(sum, sum + (n-i)))))
-#                 sum += (n-i)
-#
-# f4(3)
-# f4(0)
-# f4(1)
+f2(3)
+f2(0)
+f2(5)
+
+def f4(n):
+    sum=1;
+    for loop in range(0,2):
+        for i in range(1,n+1):
+            if(loop == 0):
+                print(*list(map(lambda x: x, range(sum, sum + i))))
+                sum += i
+            else:
+                print(*list(map(lambda x: x, range(sum, sum + (n-i)))))
+                sum += (n-i)
+
+f4(3)
+f4(0)
+f4(1)
 
 
-# def f6(matrix):
-#     print(sum(list(map(lambda x : sum(x), matrix))))
-#
-# f6([[1,0],[0,1]])
-# f6([[1,2,3],[4,5,6]])
-# f6([[1],[2],[3],[4]])
+def f6(matrix):
+    print(sum(list(map(lambda x : sum(x), matrix))))
 
-# def f8(matrix):
-#     for i in range(0, len(matrix)):
-#         print(*list(filter(lambda x:x%2==1, matrix[i])),sep=' ')
-#
-# f8([[1,0],[0,1]])
-# f8([[1,2,3],[4,5,6]])
-# f8([[1],[2],[3],[4]])
+f6([[1,0],[0,1]])
+f6([[1,2,3],[4,5,6]])
+f6([[1],[2],[3],[4]])
 
-# def f10(matrix1, matrix2):
-#     rmatrix=[]
-#     if len(matrix1[0]) == len(matrix2):
-#         for row in range(0,len(matrix1)):
-#             lst=[]
-#             value=0
-#             for col2 in range(0, len(matrix2[0])):
-#                 lst.append(sum(list(map(lambda col : matrix1[row][col]*matrix2[col][col2], range(0, len(matrix1[row]))))))
-#             rmatrix.append(lst)
-#     return rmatrix
-#
-# #f10([[1,0],[0,1]],[[1,0],[0,1]])
-# a = f10([[1,2,3],[4,5,6]],[[-1,-1],[-1,-1],[-1,-1]])
-# print(a)
+def f8(matrix):
+    for i in range(0, len(matrix)):
+        print(*list(filter(lambda x:x%2==1, matrix[i])),sep=' ')
+
+f8([[1,0],[0,1]])
+f8([[1,2,3],[4,5,6]])
+f8([[1],[2],[3],[4]])
+
+def f10(matrix1, matrix2):
+    rmatrix=[]
+    if len(matrix1[0]) == len(matrix2):
+        for row in range(0,len(matrix1)):
+            lst=[]
+            value=0
+            for col2 in range(0, len(matrix2[0])):
+                lst.append(sum(list(map(lambda col : matrix1[row][col]*matrix2[col][col2], range(0, len(matrix1[row]))))))
+            rmatrix.append(lst)
+    return rmatrix
+
+#f10([[1,0],[0,1]],[[1,0],[0,1]])
+a = f10([[1,2,3],[4,5,6]],[[-1,-1],[-1,-1],[-1,-1]])
+print(a)
 
 def f12(rows, cols):
-    print(list(map(lambda r : list(map(lambda c : r+c, range(cols))), range(rows))))
-
-
-
-f12(3,3)
-
-
-def f14(rows, cols):
     matrix=[]
 
     for r in range(0,rows):
